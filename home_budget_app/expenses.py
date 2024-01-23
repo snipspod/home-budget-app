741121eb1628b86bd26a826586892333fcabbab3
snipspod <jast99@gmail.com>
2024-01-23 22:28:13 +0100

    nihil novi

--- home_budget_app/expenses.py
+++ home_budget_app/expenses.py
@@ -29,6 +29,27 @@ def expense_history():
 
         return render_template('expenses_history.html', expenses=expenses, categories=categories, accounts=accounts)
 
+
+@bp.route('/add', methods=('GET',))
+@login_required
+def expense_add():
+     
+     from home_budget_app.db import get_user_accounts, get_user_categories
+     from datetime import datetime
+
+     categories = get_user_categories(g.user['email'])
+     accounts = get_user_accounts(g.user['email'])
+     date = datetime.now().strftime("%Y-%m-%d")
+
+     return render_template('expenses_add.html', accounts=accounts, categories=categories, date=date)
+
+
+@bp.route('/submit-expenses', methods=('POST',))
+def add_multiple_expenses():
+     back = request.referrer
+     return redirect(back)
+
+
 @bp.route('/update', methods=('POST',))
 @login_required
 def update_expense():
@@ -54,7 +75,6 @@ def update_expense():
 
     return redirect(back)
 
-# TODO: nie działa formularz drugi na stronie
 
 
 @bp.route('/delete', methods=('POST',))
--- home_budget_app/static/js/expenses.js
+++ home_budget_app/static/js/expenses.js
@@ -1,3 +1,17 @@
+let tableRows = document.querySelectorAll('.expenseTableRow')
+let amount = document.querySelector('#amount')
+let category = document.querySelector('#category')
+let date = document.querySelector('#date')
+let account = document.querySelector('#account')
+let description = document.querySelector('#description')
+let expenseId = document.querySelectorAll('#expense_id')
+
+
+let btnDelRow = document.querySelector("#btnDelRow")
+let addRowBtns = document.querySelectorAll('.btnAddRow')
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     let table = new DataTable('#expenses', {
         columnDefs: [
@@ -6,38 +20,55 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 } ,false)
 
-let tableRows = document.getElementsByClassName('expenseTableRow')
-let amount = document.querySelector('#amount')
-let category = document.querySelector('#category')
-let date = document.querySelector('#date')
-let account = document.querySelector('#account')
-let description = document.querySelector('#description')
-let expenseId = document.querySelectorAll('#expense_id')
-// let btnDeleteExpense = document.querySelector('#btnDeleteExpense')
 
-
-
-Array.from(tableRows).forEach(row => {
+tableRows.forEach(row => {
     row.addEventListener('click', () => {
         amount.value = row.dataset.amount.replace('.', ',')
         category.value = row.dataset.category
         date.value = row.dataset.date
         account.value = row.dataset.account
         description.value = row.dataset.description
-        Array.from(expenseId).forEach(field => {
+        expenseId.forEach(field => {
             field.value = row.dataset.expenseid
         })
     })
 });
 
-// btnDeleteExpense.addEventListener('click', () => {
-//     fetch('/expenses/delete', {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/x-www-form-urlencoded'
-//         },
-//         'redirect': 'follow'
-//     })
-// })
+btnDelRow.addEventListener('click', () => {
+    alert('hej')
+})
 
+addRowBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        addTableRow()
+    })
+})
 
+function addTableRow() {
+    let addExpenseTable = document.querySelector("#addExpensesTable")
+    let addExpenseTableBody = document.querySelector("#addExpensesTableBody")
+
+    console.log(addExpenseTableBody.rows.item(addExpenseTableBody.rows.length - 1))
+    console.log(parseInt(addExpenseTableBody.rows.item(addExpenseTableBody.rows.length - 1).firstElementChild.textContent) + 1)
+    let newRow = addExpenseTable.insertRow(-1)
+    let orderNumber = newRow.insertCell(0)
+
+    orderNumber.innerText = `1`
+
+    let cell2 = newRow.insertCell(1)
+    cell2.innerHTML = `<input type="text" id="amount" name="amount" placeholder="0" 
+    pattern="^(([1-9]\d{0,2}(\d{3})*)|\d+)?(,\d{1,2})?$"
+    title="Podaj wartość całkowitą, lub po przecinku np. 0,25, 30, 21,3"
+    class="form-control" required>`
+
+    let cell3 = newRow.insertCell(2)
+    cell3.innerHTML = `<input type="text" id="description" name="description" pattern="^(\S| ){1,100}$" title="Opis nie może być pusty i może zawierać maksymalnie 100 znaków" class="form-control">`
+
+    let cell4 = newRow.insertCell(3)
+    cell4.innerHTML = ``
+    
+    let cell5 = newRow.insertCell(4)
+    let cell6 = newRow.insertCell(5)
+    let cell7 = newRow.insertCell(6)
+
+}
